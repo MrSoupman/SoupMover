0590781fb5ce47c16196e364854ebbfe75e62dcc
MrSoupman <ioxflash@gmail.com>
2022-04-10 01:40:41 -0400

    Fixed gifs not loading properly

--- ViewModels/PreviewViewModel.cs
+++ ViewModels/PreviewViewModel.cs
@@ -246,16 +246,16 @@ namespace SoupMover.ViewModels
         }
         private BitmapImage LoadBitmapImage(string fileName)
         {
-            using (var stream = new FileStream(fileName, FileMode.Open))
-            {
-                var bitmapImage = new BitmapImage();
-                bitmapImage.BeginInit();
-                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
-                bitmapImage.StreamSource = stream;
-                bitmapImage.EndInit();
-                bitmapImage.Freeze(); // just in case you want to load the image in another thread
-                return bitmapImage;
-            }
+            var stream = new FileStream(fileName, FileMode.Open);
+            
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.StreamSource = stream;
+            bitmapImage.EndInit();
+            bitmapImage.Freeze(); // just in case you want to load the image in another thread
+            return bitmapImage;
+            
         }
         private void HidePreviews()
         {
@@ -295,7 +295,8 @@ namespace SoupMover.ViewModels
                         if (MimeTypeMap.GetMimeType(uri.ToString()).Contains("gif"))
                         {
                             GifVisible = true;
-                            Gif = LoadBitmapImage(HVM.SelectedFile);
+                            var test = LoadBitmapImage(HVM.SelectedFile);
+                            Gif = test;
                         }
                         else
                         {
